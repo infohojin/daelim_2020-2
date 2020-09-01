a33597fd0478646be31a8ec7632e7ea45bce0d50
scian0204 <scian0204@gmail.com>
2020-09-01 12:38:31 +0900

    hello1.php 실습

---
+++ 201930107/hello1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>대림대학교</h1>
+    <?php
+        echo "201930107 남궁찬입니다.";
+    ?>
+</body>
+</html>
