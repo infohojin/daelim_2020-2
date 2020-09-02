bbf144be99151507e330057faa238fba54d6a6d2
zxcv12045 <62276806+zxcv12045@users.noreply.github.com>
2020-09-02 14:31:57 +0900

    201930435 홍예연 1, 2주차 퀴즈 (#69)
    * hello 201930435
    
    * hello 종료코드 기록
    
    * comment 실습
    
    * 1, 2주차 퀴즈

---
+++ 201930435/hello2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>대림대학교</h1>
+  <?php
+    echo "201930435 홍예연 입니다.";
+  ?>
+
+  <?php
+    echo "만나서 반갑습니다.";
+  ?>
+</body>
+</html>
